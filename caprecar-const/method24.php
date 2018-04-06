07cd405ccb7603fbeae609417ed31c7c6a8b754a
Prion84 <36615554+Prion84@users.noreply.github.com>
2018-04-06 18:15:46 +0700

    Create method24.php

---
+++ caprecar-const/method24.php
@@ -0,0 +1,40 @@
+ <form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
+	
+	Input Number: <input type="text" name="number" /><br />
+	<input type="submit" value="Submit" />
+</form>
+
+ <?php
+    
+$x=$_POST['number'];
+//var_dump(empty($x));
+if (isset($x)!=='true') {
+   // $x=$_POST['number'];
+$i=1; 
+echo 'Define Kaprekar Constant<br>'; 
+echo $x.'<br>'; 
+while ($x!==6174&&$i<16) 
+{ $x=(string)$x;
+  if(strlen($x)==3) {$x='0'.$x;}
+ echo $x.'<br>';
+$y00=str_split($x);
+sort($y00);
+$y=implode($y00);	
+rsort($y00);	
+$z=implode($y00);
+ 
+ if (($x[0]==$x[1])&&($x[2]==$x[3])) 
+{echo 'change number<br>'; 
+break;} 
+if ($y>$z) 
+{$r=$y-$z; 
+echo $y.'-'.$z.'='.$r.'<br>';} 
+else {$r=$z-$y; 
+echo $z.'-'.$y.'='.$r.'<br>';} 
+$x=$r; 
+$i++; 
+echo '------<br>';
+}
+}
+
+?>
