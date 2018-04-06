43e81c888929cdef63971bf14081ab216e15ad89
Prion84 <36615554+Prion84@users.noreply.github.com>
2018-04-06 18:57:07 +0700

    Update method24.php

--- caprecar-const/method24.php
+++ caprecar-const/method24.php
@@ -1,26 +1,24 @@
- <form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
-	
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">	
 	Input Number: <input type="text" name="number" /><br />
 	<input type="submit" value="Submit" />
 </form>
-
  <?php
 if (isset($_POST['number'])) {
  $x=$_POST['number'];
 $i=1; 
-echo 'Define Caprecar Constant<br>'; 
+echo 'Define Kaprekar Constant<br>'; 
 echo $x.'<br>'; 
 while ($x!==6174&&$i<16) 
 { $x=(string)$x;
   if(strlen($x)==3) {$x='0'.$x;}
- echo $x.'<br>';
+ /*echo $x.'<br>';*/
 $y00=str_split($x);
 sort($y00);
 $y=implode($y00);	
 rsort($y00);	
 $z=implode($y00);
  
- if (($x[0]==$x[1])&&($x[2]==$x[3])) 
+ if (($x[0]==$x[1])&&($x[2]==$x[3])&&($x[0]==$x[2])&&($x[1]==$x[3])) 
 {echo 'change number<br>'; 
 break;} 
 if ($y>$z) 
