13fa2d87d0d24045ae6ad3aaf5a50dbd95641698
Prion84 <36615554+Prion84@users.noreply.github.com>
2018-04-06 14:40:22 +0700

    Create method23.php

---
+++ caprecar-const/method23.php
@@ -0,0 +1,29 @@
+<?php 
+$x=111; 
+$i=1; 
+echo 'Define Kaprekar Constant<br>'; 
+echo $x.'<br>'; 
+while ($x!==6174&&$i<16) 
+{ $x=(string)$x;
+ // echo strlen($x);
+  if(strlen($x)==3) {$x='0'.$x;}
+ echo $x.'<br>';
+$y00=str_split($x);
+sort($y00);
+$y=implode($y00);	
+rsort($y00);	
+$z=implode($y00);
+ 
+ if (($x[0]==$x[1])&&($x[2]==$x[3])) 
+{echo 'change number<br>'; 
+break;} 
+if ($y>$z) 
+{$r=$y-$z; 
+echo $y.'-'.$z.'='.$r.'<br>';} 
+else {$r=$z-$y; 
+echo $z.'-'.$y.'='.$r.'<br>';} 
+$x=$r; 
+$i++; 
+echo '------<br>';
+}
+?>
