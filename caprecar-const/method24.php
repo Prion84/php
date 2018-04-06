0cf4c0d119ce9d5580998ca9bb284ea45659d288
Prion84 <36615554+Prion84@users.noreply.github.com>
2018-04-06 19:31:00 +0700

    Update method24.php

--- caprecar-const/method24.php
+++ caprecar-const/method24.php
@@ -1,33 +1,51 @@
-<form action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">	
+
+<form action="<?php
+$_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">	
 	Input Number: <input type="text" name="number" /><br />
 	<input type="submit" value="Submit" />
 </form>
  <?php
-if (isset($_POST['number'])) { //don't count if number isn't define in form
- $x=$_POST['number'];
-$i=1; 
-echo 'Define Kaprekar Constant<br>'; 
-echo $x.'<br>'; 
-while ($x!==6174&&$i<10) //Count until x=6174 or i<10
-{ $x=(string)$x;//Define type String for variable x
-  if(strlen($x)==3) {$x='0'.$x;} // add 0 for 3-signs number
-$y00=str_split($x);// Split String in array
-sort($y00);// Sort Array for largest number
-$y=implode($y00);//Combine Array in one String/Number	
-rsort($y00);//Reverse sort  arrayfor smallest	
-$z=implode($y00); //Combine Array in one String/Number	
- if (($x[0].$x[1])==($x[2].$x[3])) //check Number with same digits
-{echo 'change number<br>'; 
-break;} 
-if ($y>$z) 
-{$r=$y-$z; 
-echo $y.'-'.$z.'='.$r.'<br>';} 
-else {$r=$z-$y; 
-echo $z.'-'.$y.'='.$r.'<br>';} 
-$x=$r; 
-$i++; 
-echo '------<br>';
-}
-}
+
+if (isset($_POST['number']))
+	{ //don't count if number isn't define in form
+	$x = $_POST['number'];
+	$i = 1;
+	echo 'Define Kaprekar Constant<br />';
+	echo $x . '<br />';
+	while ($x !== 6174 && $i < 10) //Count until x=6174 or i<10
+		{
+		$x = (string)$x; //Set type String for variable x
+		if (strlen($x) == 3)
+			{
+			$x = '0' . $x;
+			} // add 0 for 3-digitals number
+		$y00 = str_split($x); // Split String in array
+		sort($y00); // Sort Array for largest number
+		$y = implode($y00); //Combine Array in one String/Number
+		rsort($y00); //Reverse sort  array for smallest Number
+		$z = implode($y00); //Combine Array in one String/Number
+		if (($x[0] . $x[1]) == ($x[2] . $x[3])) //check Number with same digits
+			{
+			echo 'change number<br />';
+			break;
+			}
+
+		if ($y > $z)
+			{
+			$r = $y - $z;
+			echo $y . '-' . $z . '=' . $result . '<br />';
+			}
+		  else
+			{
+			$r = $z - $y;
+			echo $z . '-' . $y . '=' . $result . '<br />';
+			}
+
+		$x = $result;
+		$i++;
+		echo '------<br />';
+		}
+	}
 
 ?>
+
